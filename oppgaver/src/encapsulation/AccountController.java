998b776beb42cfd78911d5d077f82356a49fe4cd
Vegard Hellem <vhellem@gmail.com>
2017-01-19 17:11:42 +0100

    Added Vehicle GUI files, and updated Account

--- oppgaver/src/encapsulation/AccountController.java
+++ oppgaver/src/encapsulation/AccountController.java
@@ -1,6 +1,10 @@
 package encapsulation;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -23,6 +27,8 @@ public class AccountController {
 	private TextField withdrawTextField; 
 	@FXML
 	private TextField setInterestRateTextField;
+	
+	@FXML private TextArea exceptionTextArea;
 
 	@FXML
 	private void initialize() {
@@ -42,6 +48,19 @@ public class AccountController {
 	
 	private double getDoubleFromTextField(TextField textField) {
 		return Double.parseDouble(textField.getText());
+	}
+	
+	private void showException(Exception e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		pw.write(e.getMessage() != null ? e.getMessage() : "No message");
+		pw.write("\n\n");
+		e.printStackTrace(pw);
+		exceptionTextArea.setText(sw.toString());
+	}
+	
+	private void clearException() {
+		exceptionTextArea.setText("");
 	}
 	
 
