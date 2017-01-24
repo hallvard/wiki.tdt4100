b98ef5e2fca1ce67af648fdda15fb42c1dc61f2b
Nils Barlaug <nils.barlaug@gmail.com>
2017-01-24 14:29:30 +0100

    Remove exception handling to reduce controller complexity

--- oppgaver/src/encapsulation/VehicleController.java
+++ oppgaver/src/encapsulation/VehicleController.java
@@ -1,14 +1,6 @@
 package encapsulation;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.function.Consumer;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class VehicleController {
@@ -23,15 +15,11 @@ public class VehicleController {
 	@FXML private TextField setFuelTextField;
 	@FXML private TextField setRNTextField;
 	
-	@FXML private TextArea exceptionTextArea;
-	
 	@FXML
 	private void initialize() {
-		
 		getVehicleTextField.setDisable(true);
 		getFuelTextField.setDisable(true);
 		getRNTextField.setDisable(true);
-		
 	}
 	
 	private void update() {
@@ -40,52 +28,14 @@ public class VehicleController {
 		getRNTextField.setText(vehicle.getRegistrationNumber());
 		
 	}
-	
-	private void showException(Exception e) {
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		pw.write(e.getMessage() != null ? e.getMessage() : "No message");
-		pw.write("\n\n");
-		e.printStackTrace(pw);
-		exceptionTextArea.setText(sw.toString());
-	}
-	
-	private void clearException() {
-		exceptionTextArea.setText("");
-	}
-	
-	private void tryvehicleManipulation(Consumer<Vehicle> vehicleConsumer) {
-		try {
-			vehicleConsumer.accept(vehicle);
-		}
-		catch (Exception e) {
-			showException(e);
-			throw e;
-		}
-		finally {
-			update();
-		}
-		
-		clearException();	
-	}
-	
+
 	@FXML
 	private void create() {
 		char type = setVehicleTextField.getText().charAt(0);
 		char fuel = setFuelTextField.getText().charAt(0);
 		String rn = setRNTextField.getText();
-		try {
-			vehicle = new Vehicle(type, fuel, rn); 
-		}
-		catch(Exception e) {
-			showException(e); 
-			throw e; 
-		}
-		finally {
-			update(); 
-		}
-		clearException(); 
-		
+		vehicle = new Vehicle(type, fuel, rn); 
+		update(); 
 	}
 	
 
