0777ed7fb6c5220e0aaaa63f075c8ea5c963f9d3
Vemund M Santi <vemund@santi.no>
2017-02-15 11:42:52 +0100

    Update .ex files for assignment 5

--- oppgaver/tests/objectstructures/StopWatch.ex
+++ oppgaver/tests/objectstructures/StopWatch.ex
@@ -1,28 +1,36 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<exercise:Exercise xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:exercise="platform:/plugin/no.hal.learning.exercise.model/model/exercise.ecore" xmlns:jdt="platform:/plugin/no.hal.learning.exercise.jdt/model/jdt-exercise.ecore" xmlns:junit="platform:/plugin/no.hal.learning.exercise.junit/model/junit-exercise.ecore"
-	xmlns:workbench="platform:/plugin/no.hal.learning.exercise.workbench/model/workbench-exercise.ecore">
-  <parts xsi:type="exercise:ExercisePart" title="StopWatch and StopWatchManager">
-    <tasks xsi:type="exercise:Task">
-      <q xsi:type="exercise:StringQuestion" question="Write source code for the StopWatch class."/>
-      <a xsi:type="jdt:JdtSourceEditAnswer" className="objectstructures.StopWatch"/>
-    </tasks>
-    <tasks xsi:type="exercise:Task">
-      <q xsi:type="exercise:StringQuestion" question="Write source code for the StopWatchManager class."/>
-      <a xsi:type="jdt:JdtSourceEditAnswer" className="objectstructures.StopWatchManager"/>
-    </tasks>
-    <tasks xsi:type="exercise:Task">
-      <q xsi:type="exercise:StringQuestion" question="Test the StopWatchManager class, by running its main method."/>
-      <a xsi:type="jdt:JdtLaunchAnswer" className="objectstructures.StopWatchManager"/>
-    </tasks>
-    <tasks xsi:type="exercise:Task">
-      <q xsi:type="exercise:StringQuestion" question="Run the StopWatchManagerTest."/>
-      <a xsi:type="junit:JunitTestAnswer" testRunName="objectstructures.StopWatchManagerTest"/>
-    </tasks>
-  </parts>
-  <parts xsi:type="exercise:ExercisePart" title="Tool usage">
+<xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:exercise="platform:/plugin/no.hal.learning.exercise.model/model/exercise.ecore" xmlns:jdt="platform:/plugin/no.hal.learning.exercise.jdt/model/jdt-exercise.ecore" xmlns:junit="platform:/plugin/no.hal.learning.exercise.junit/model/junit-exercise.ecore" xmlns:workbench="platform:/plugin/no.hal.learning.exercise.workbench/model/workbench-exercise.ecore">
+  <exercise:Exercise>
+    <parts xsi:type="exercise:ExercisePart" title="StopWatch and StopWatchManager">
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Write source code for the StopWatch class."/>
+        <a xsi:type="jdt:JdtSourceEditAnswer" className="objectstructures.StopWatch"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Write source code for the StopWatchManager class."/>
+        <a xsi:type="jdt:JdtSourceEditAnswer" className="objectstructures.StopWatchManager"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Test the StopWatch class, by running its main method."/>
+        <a xsi:type="jdt:JdtLaunchAnswer" className="objectstructures.StopWatch"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Test the StopWatchManager class, by running its main method."/>
+        <a xsi:type="jdt:JdtLaunchAnswer" className="objectstructures.StopWatchManager"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Run the StopWatchTest."/>
+        <a xsi:type="junit:JunitTestAnswer" testRunName="objectstructures.StopWatchTest"/>
+      </tasks>
+      <tasks xsi:type="exercise:Task">
+        <q xsi:type="exercise:StringQuestion" question="Run the StopWatchManagerTest."/>
+        <a xsi:type="junit:JunitTestAnswer" testRunName="objectstructures.StopWatchManagerTest"/>
+      </tasks>
+    </parts>
+    <parts xsi:type="exercise:ExercisePart" title="Tool usage">
       <tasks xsi:type="exercise:Task">
         <q xsi:type="exercise:StringQuestion" question="Use breakpoints to debug code."/>
-        <a xsi:type="workbench:DebugEventAnswer" action="suspend.breakpoint" elementId="objectstructures.StopWatch*"/>
+        <a xsi:type="workbench:DebugEventAnswer" elementId="objectstructures.StopWatch*" action="suspend.breakpoint"/>
       </tasks>
       <tasks xsi:type="exercise:Task">
         <q xsi:type="exercise:StringQuestion" question="Use the debug command Step Over"/>
@@ -36,5 +44,6 @@
         <q xsi:type="exercise:StringQuestion" question="Use the Variables view"/>
         <a xsi:type="workbench:PartTaskAnswer" elementId="org.eclipse.debug.ui.VariableView" action="activated"/>
       </tasks>
-  </parts>
-</exercise:Exercise>
+    </parts>
+  </exercise:Exercise>
+</xmi:XMI>
