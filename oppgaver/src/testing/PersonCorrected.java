e083fcd077bdeb64bc4fc2d669ff0968d7bfb470
Torjus Iveland <torjusti@gmail.com>
2018-03-15 21:00:13 +0100

    Update package

--- oppgaver/src/testing/PersonCorrected.java
+++ oppgaver/src/testing/PersonCorrected.java
@@ -1,4 +1,4 @@
-package minegenkode;
+package testing;
 
 import java.util.ArrayList;
 import java.util.List;
--- oppgaver/src/testing/PersonTest.java
+++ oppgaver/src/testing/PersonTest.java
@@ -1,4 +1,4 @@
-package minegenkode;
+package testing;
 
 import org.junit.Before;
 import org.junit.Test;
