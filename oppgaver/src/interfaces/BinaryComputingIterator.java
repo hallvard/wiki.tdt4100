af1629e90df56dfe290438afb4f9f8dd9c70b0c1
Hallvard Traetteberg <hal@ntnu.no>
2018-05-16 13:10:09 +0200

    Fikset feil i LF

--- oppgaver/src/interfaces/BinaryComputingIterator.java
+++ oppgaver/src/interfaces/BinaryComputingIterator.java
@@ -10,26 +10,26 @@ public class BinaryComputingIterator implements Iterator<Double> {
 	private final Double default1, default2;
 	private final BinaryOperator<Double> operator;
 
-	public BinaryComputingIterator(Iterator<Double> iterator1, Iterator<Double> iterator2, Double default1, Double default2, BinaryOperator<Double> operator) {
+	public BinaryComputingIterator(final Iterator<Double> iterator1, final Iterator<Double> iterator2, final Double default1, final Double default2, final BinaryOperator<Double> operator) {
 		this.iterator1 = iterator1;
 		this.iterator2 = iterator2;
 		this.default1 = default1;
 		this.default2 = default2;
 		this.operator = operator;
 	}
-	public BinaryComputingIterator(Iterator<Double> iterator1, Iterator<Double> iterator2, BinaryOperator<Double> operator) {
+	public BinaryComputingIterator(final Iterator<Double> iterator1, final Iterator<Double> iterator2, final BinaryOperator<Double> operator) {
 		this(iterator1, iterator2, null, null, operator);
 	}
 
 	@Override
 	public boolean hasNext() {
-		return (iterator1.hasNext() || default1 != null) && (iterator2.hasNext() || default2 != null) && (iterator1.hasNext() || iterator1.hasNext());
+		return (iterator1.hasNext() || default1 != null) && (iterator2.hasNext() || default2 != null) && (iterator1.hasNext() || iterator2.hasNext());
 	}
 
 	@Override
 	public Double next() {
-		Double next1 = (iterator1.hasNext() ? iterator1.next() : default1);
-		Double next2 = (iterator2.hasNext() ? iterator2.next() : default2);
+		final Double next1 = (iterator1.hasNext() ? iterator1.next() : default1);
+		final Double next2 = (iterator2.hasNext() ? iterator2.next() : default2);
 		return operator.apply(next1, next2);
 	}
 
