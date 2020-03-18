1ae610cb75ba1ac6ef3db088ab8bcd2ad82821ee
Ole-Magnus Pedersen <pedersen.olemagnus@gmail.com>
2020-03-18 14:06:52 +0100

    Fjernet LF på øvinger

---
+++ oppgaver/src/patterns/observable/Readme.md
@@ -0,0 +1,1 @@
+Kode er fjernet.
--- oppgaver/src/patterns/observable/StopWatchListener.java
+++
@@ -1,6 +0,0 @@
-package patterns.observable;
-
-public interface StopWatchListener {
-	int TICKED = 1, STARTED = 2, LAPPED = 4, STOPPED = 8;
-	void stopWatchChanged(StopWatch stopWatch, int what);
-}
